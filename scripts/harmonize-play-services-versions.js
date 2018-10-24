a250ee075854c9f900f7877dea34231805dc17cd
David Silvestre <david.silvestre@appfeel.com>
2018-10-24 16:19:24 +0200

    Restructure hooks code

--- scripts/harmonize-play-services-versions.js
+++ scripts/harmonize-play-services-versions.js
@@ -1,139 +1,174 @@
 #!/usr/bin/env node
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const dependencies = require(path.resolve(process.cwd(), 'plugins/cordova-play-services-version-adapter/scripts/dependencies.json'));
-const cordovaVersion = require(path.resolve(process.cwd(), 'platforms/android/cordova/version'));
+const properties = path.resolve(process.cwd(), 'platforms/android/project.properties');
 const cordovaLybrary = 'cordova.system.library';
 const googleAndroid = 'com.google.android.gms';
 const googleFirebase = 'com.google.firebase';
-const CORDOVA_VERSIONS = {
-    6: {
-        name: 'cordova-android@6',
-        properties: 'platforms/android/project.properties'
-    },
-    7: {
-        name: 'cordova-android@7',
-        properties: 'platforms/android/project.properties'
-    }
-};
-
+const playServicesAds = 'play-services-ads';
+const compatPlgs = ['cordova-admob'];
+const libraries = [];
 
 let deferral;
+let isCompatible = false;
+let hasPlayServicesAds = false;
+let minVersion;
+let newVersion;
 
-function getBiggerVersion(v1, v2) {
+function isAdmobAndCompatible(package, dependency) {
+    if (package === googleAndroid && dependency === playServicesAds) {
+        hasPlayServicesAds = true;
+        if (!isCompatible) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function getBiggerVersion(v1, v2, idx = 0) {
     const v1Params = v1.split('.');
     const v2Params = v2.split('.');
-    if (!isNaN(v1Params[0]) && !isNaN(v2Params[0])) {
-        if (parseInt(v1Params[0]) < parseInt(v2Params[0])) {
+    if (!isNaN(v1Params[idx]) && !isNaN(v2Params[idx])) {
+        if (parseInt(v1Params[idx]) < parseInt(v2Params[idx])) {
             return v2;
+        } else if (parseInt(v1Params[idx]) === parseInt(v2Params[idx])) {
+            if (idx === v1Params.length - 1) {
+                return v1;
+            }
+            return getBigger(v1, v2, idx + 1);
         }
-    } else if (isNaN(v1Params[0]) && !isNaN(v2Params[0])) {
+        return v1;
+    } else if (isNaN(v1Params[idx]) && !isNaN(v2Params[idx])) {
         return v2;
     }
     return v1;
 }
 
-function run() {
-    const version = parseInt(cordovaVersion.version);
-    const properties = CORDOVA_VERSIONS[version].properties;
-    const data = fs.readFileSync(properties, 'utf8');
-    if (data) {
-        const actualLines = data.split('\n');
-        const libraries = [];
-        let minVersion;
-        const newLines = [];
-        for (let i = 0, n = actualLines.length; i < n; i += 1) {
-            const actualLine = actualLines[i];
-            if (actualLine.indexOf(cordovaLybrary) > -1) {
-                const lineParams = actualLine.split('=');
-                const libraryParams = lineParams[1].split(':');
-                const package = libraryParams[0];
-                const dependency = libraryParams[1];
-                const version = libraryParams[2];
-                if (package.indexOf(googleAndroid) > -1 || package.indexOf(googleFirebase) > -1) {
-                    libraries.push({ library: lineParams[0], package, dependency, version, isGoogle: true });
-                    if (!minVersion) {
-                        minVersion = { package, dependency, version };
-                    } else {
-                        if (getBiggerVersion(minVersion.version, version) === version) {
-                            minVersion = {
-                                package,
-                                dependency,
-                                version: getBiggerVersion(minVersion.version, version),
-                            };
-                        }
-                    }
+function prepareLibraries(lines) {
+    const newLines = [];
+    for (let i = 0, n = lines.length; i < n; i += 1) {
+        const line = lines[i];
+        if (line.indexOf(cordovaLybrary) > -1) {
+            const params = line.split('=');
+            const libraryParams = params[1].split(':');
+            const package = libraryParams[0];
+            const dependency = libraryParams[1];
+            const version = libraryParams[2];
+            if ((package.indexOf(googleAndroid) > -1 || package.indexOf(googleFirebase) > -1) && isAdmobAndCompatible(package, dependency)) {
+                libraries.push({ library: params[0], package, dependency, version, isGoogle: true });
+                if (!minVersion) {
+                    minVersion = { package, dependency, version };
                 } else {
-                    libraries.push({ library: lineParams[0], package, dependency, version, isGoogle: false });
+                    const bigger = getBiggerVersion(minVersion.version, version);
+                    if (bigger === version) {
+                        minVersion = { package, dependency, version: bigger };
+                    }
                 }
             } else {
-                newLines.push(actualLine);
+                libraries.push({ library: params[0], package, dependency, version, isGoogle: false });
+            }
+        } else {
+            newLines.push(line);
+        }
+    }
+    return newLines;
+}
+
+function setNewVersion(version, libraries) {
+    if (version.dependencies.filter(d => libraries.indexOf(d) > -1).length === libraries.length) {
+        newVersion = version.version;
+        return true;
+    }
+    return false;
+}
+
+function prepareNewVersion() {
+    let possibleVersions = dependencies.filter(l => getBiggerVersion(l.version, minVersion.version) === l.version);
+    const parsedLibraries = libraries.filter(l => l.isGoogle).map(l => `${l.package}:${l.dependency}`);
+    if (isNaN(minVersion.version.split('.')[0])) {
+        for (let i = possibleVersions.length - 1, n = 0; i >= n; i -= 1) {
+            const setted = setNewVersion(possibleVersions[i], parsedLibraries);
+            if (setted) {
+                break;
             }
         }
-        let error;
-        if (libraries.length > 0) {
-            const possibleVersions = dependencies.filter(l => getBiggerVersion(l.version, minVersion.version) === l.version);
-            const parsedLibraries = libraries.filter(l => l.isGoogle).map(l => {
-                if (l.isGoogle) {
-                    return `${l.package}:${l.dependency}`;
-                }
-            });
-            const dependency = `${minVersion.package}:${minVersion.dependency}`;
-            const version = `${minVersion.version.split('.')[0]}`;
-            let newVersion;
-            if (isNaN(minVersion.version.split('.')[0])) {
-                for (let i = possibleVersions.length - 1, n = 0; i >= n; i -= 1) {
-                    const possibleVersion = possibleVersions[i];
-                    if (possibleVersion.dependencies.filter(d => parsedLibraries.indexOf(d) > -1).length === parsedLibraries.length) {
-                        newVersion = possibleVersion.version;
-                        break;
-                    }
-                }
-            } else {
-                for (let i = 0, n = possibleVersions.length; i < n; i += 1) {
-                    const possibleVersion = possibleVersions[i];
-                    if (possibleVersion.dependencies.filter(d => parsedLibraries.indexOf(d) > -1).length === parsedLibraries.length) {
-                        newVersion = possibleVersion.version;
-                        break;
-                    }
-                }
+    } else {
+        for (let i = 0, n = possibleVersions.length; i < n; i += 1) {
+            const setted = setNewVersion(possibleVersions[i], parsedLibraries);
+            if (setted) {
+                break;
             }
+        }
+    }
+    if (!newVersion) {
+        const parentVersion = `${minVersion.version.split('.')[0]}.0.0`;
+        possibleVersions = dependencies.filter(l => l.version >= parentVersion && l.version < minVersion.version);
+        for (let i = possibleVersions.length - 1, n = 0; i >= n; i -= 1) {
+            const setted = setNewVersion(possibleVersions[i], parsedLibraries);
+            if (setted) {
+                break;
+            }
+        }
+    }
+}
+
+function prepareSuccess(libraries, version) {
+    let success = `\'Cordova Play Services Version Adapter\' has successfully applied the following changes:\n    - '`;
+    success += `${libraries.join(`' => ${version}\n    - '`)}' => ${version}`;
+    process.env.adapterSuccess = success;
+}
+
+function prepareWarning() {
+    let warning = '';
+    if (hasPlayServicesAds) {
+        warning += 'WARNING: some plugins are using \'com.google.android.gms:play-services-ads\''
+        warning += '\nIt is not possible to find the required version of \'Google Play Services\' Ads library.';
+        warning += `\n\nUse 'cordova-admob' to monetize with AdMob`;
+    } else if (!hasPlayServicesAds) {
+        warning += 'Monetize your app with AdMob ads. Now available with this cordova / phonegap plugin:';
+    }
+    warning += '\ncordova plugin add cordova-admob';
+    warning += '\nDocs: https://github.com/appfeel/admob-google-cordova';
+    process.env.adapterWarning = warning;
+}
+
+function prepareError(libraries) {
+    let error;
+    error = '\'Cordova Play Services Version Adapter\' has detected an error. The following dependencies of \'Google Play Services\' are not compatible:\n';
+    error += 'You have an incompatibility of versions with the following dependencies of \'Google Play Services\':\n    - \'';
+    error += `${libraries.join('\'\n    - \'')}'\n`;
+    error += '\nIt doesn\'t exists a version of \'Google Play Services\' that includes all these dependencies.';
+    error += `\nAll dependencies of 'Google Play Services' must have the same version.`;
+    error += `\nThe application may not compile.`;
+    process.env.adapterError = error;
+}
+
+function run() {
+    const data = fs.readFileSync(properties, 'utf8');
+    if (data) {
+        const newLines = prepareLibraries(data.split('\n'));
+        if (libraries.filter(l => l.isGoogle).length > 1) {
+            prepareNewVersion();
             if (newVersion) {
                 for (let i = 0, n = libraries.length; i < n; i += 1) {
                     const library = libraries[i];
                     const libraryVersion = library.isGoogle ? newVersion : library.version;
                     newLines.push(`${library.library}=${library.package}:${library.dependency}:${libraryVersion}`);
                 }
-                const parsedLibrariesVersion = libraries.filter(l => l.isGoogle && l.version !== newVersion).map(l => {
-                    if (l.isGoogle && l.version !== newVersion) {
-                        return `${l.package}:${l.dependency}:${l.version}`;
-                    }
-                });
+                const parsedLibrariesVersion = libraries.filter(l => l.isGoogle && l.version !== newVersion).map(l => `${l.package}:${l.dependency}:${l.version}`);
                 if (parsedLibrariesVersion.length > 0) {
-                    let success = `\'Cordova Play Services Version Adapter\' plugin has made changes in the following dependencies:\n    - '`;
-                    success += `${parsedLibrariesVersion.join(`' => ${newVersion}\n    - '`)}' => ${newVersion}\n`;
-                    success += `\nThe library '${dependency}' needs at least version ${version} of 'Google Play Services'`;
-                    success += '\nThese changes are necessary since all plugins must use the same version of \'Google Play Services\'';
-                    success += '\nOtherwise the application would not compile';
-                    process.env.adapterSuccess = success;
+                    prepareSuccess(parsedLibrariesVersion, newVersion);
                 }
             } else {
-                const parsedLibrariesVersion = libraries.filter(l => l.isGoogle).map(l => {
-                    if (l.isGoogle) {
-                        return `${l.package}:${l.dependency}:${l.version}`;
-                    }
-                });
-                error = '\'Cordova Play Services Version Adapter\' plugin detects an error: your application will not compile\n';
-                error += 'You have an incompatibility of versions with the following dependencies of \'Google Play Services\':\n    - \'';
-                error += `${parsedLibrariesVersion.join('\'\n    - \'')}'\n`;
-                error += '\nAll dependencies of \'Google Play Services\' must have the same version';
-                error += `\nThe library '${dependency}' needs at least version ${version} of 'Google Play Services'`;
-                error += `\nTried to adapt this versions but there is no version of 'Google Play Services' equal to or greater than ${version} that contains all those dependencies together`;
-                error += `\nA smaller version can not be applied because the library '${dependency}' needs functionalities that were implemented from the ${version} version`;
-                process.env.adapterError = error;
+                const parsedLibrariesVersion = libraries.filter(l => l.isGoogle).map(l => `${l.package}:${l.dependency}:${l.version}`);
+                prepareError(parsedLibrariesVersion);
             }
         }
-        if (!error) {
+        if (!isCompatible) {
+            prepareWarning();
+        }
+        if (newVersion) {
             fs.writeFileSync(properties, newLines.join('\n'));
         }
     }
@@ -153,6 +188,7 @@ function attempt(fn) {
 module.exports = function (ctx) {
     deferral = ctx.requireCordovaModule('q').defer();
     if (ctx.cmdLine.indexOf('platform add') === -1) {
+        isCompatible = ctx.opts.cordova.plugins.find(p => compatPlgs.indexOf(p) > -1);
         attempt(run)();
     } else {
         deferral.resolve();
